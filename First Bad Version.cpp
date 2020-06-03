d4e76da2b0e51feb08b9d8789943ad0b2306258e
Burhanuddin Kangsiwala <60056452+burhankangsi@users.noreply.github.com>
2020-06-03 13:50:47 +0530

    Create First Bad Version.cpp

---
+++ First Bad Version.cpp
@@ -0,0 +1,21 @@
+int firstBadVersion(int n) {
+        
+        int low = 1;
+        int high = n;
+        
+        while(low <= high)
+        {
+            int mid = low + (high - low)/2;
+            
+            if(isBadVersion(mid) == false)
+            {
+                low = mid + 1;
+            }
+            else if(isBadVersion(mid) == true)
+            {
+                high = mid - 1;
+            }
+        }
+        
+        return high + 1;
+    }
